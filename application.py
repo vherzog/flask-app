648ea06b7d87f66e1ec062af7dad49eb5c7e2c6d
Veronica Herzog <vherzog92@gmail.com>
2020-04-07 19:23:39 -0400

    add static/

--- application.py
+++ application.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+
+app.secret_key = 'chesneyandozy'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 application = Flask(__name__)
 application.debug = True
@@ -14,6 +17,18 @@ def about():
 @application.route('/welcome/<name>', methods=['GET'])
 def welcome(name):
     return '<h1>Hello there, ' + name + '!</h1>'
+
+@application.rote('/session')
+def session():
+    count = session.get('count', None)
+    if not count:
+        session['count'] = 1
+    elif count >= 10:
+        session.clear()
+        return "The session has been cleared"
+    else:
+        session['count'] += 1
+        return "The total number of refreshes for this user is: " + string(sesssion['count'])
 
 if __name__ == "__main__":
     application.run()
