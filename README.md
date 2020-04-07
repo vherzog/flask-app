2685512065cc398c31deb8dd8a70a13a4cc6ed16
Veronica Herzog <vherzog92@gmail.com>
2020-04-07 16:20:01 -0400

    add templates

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Experimenting with flask
 ## Developer: Veronica Herzog
 
 
-Following: https://medium.com/@miloharper/a-beginner-s-guide-to-creating-your-first-python-website-using-flask-aws-ec2-elastic-beanstalk-6a82b9be25e0
+Following:
+* https://medium.com/@miloharper/a-beginner-s-guide-to-creating-your-first-python-website-using-flask-aws-ec2-elastic-beanstalk-6a82b9be25e0
+* https://medium.com/better-programming/building-your-first-website-with-flask-part-2-6324721be2ae
--- application.py
+++ application.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 application.debug = True
 
 @application.route('/', methods=['GET'])
-def hello():
-    return '<p>Hello world!</p>'
+def home():
+    return render_template("home.html")
+
+@application.route('/about', methods=['GET'])
+def about():
+    return render_template("about.html")
+
+@application.route('/welcome/<name>', methods=['GET'])
+def welcome(name):
+    return '<h1>Hello there, ' + name + '!</h1>'
 
 if __name__ == "__main__":
     application.run()
